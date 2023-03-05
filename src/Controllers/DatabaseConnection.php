8916c15e0eda5349624d3561fe896265220b385d
bouimust <moussovic@gmail.com>
2023-03-05 19:50:47 +0100

    using environment variables

--- src/Controllers/DatabaseConnection.php
+++ src/Controllers/DatabaseConnection.php
@@ -1,23 +1,37 @@
 <?php
 // DATABASECONNECTION CONTROLLER
 // Config controller to connect to 'Portfolio' MySQL database, using PDO (PHP Data Objects)
+// Using environment variables for credentials
 
 namespace App\Controllers;
 
 use PDO;
+use Dotenv\Dotenv;
+
 
 class DatabaseConnection
 {
     // DATABASE CREDENTIALS
-    private const DB_HOST = "localhost";
-    private const DB_NAME = "portfolio";
-    private const DB_USER = "root";
-    private const DB_PASSWORD = "";
+    private $DB_HOST;
+    private $DB_NAME;
+    private $DB_USER;
+    private $DB_PASSWORD;
+    public Dotenv $dotenv;
+
+    public function __construct()
+    {
+        $this->dotenv = Dotenv::createImmutable($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'portfolio');
+        $this->dotenv->load();
+        $this->DB_HOST = $_ENV['DB_HOST'];
+        $this->DB_NAME = $_ENV['DB_NAME'];
+        $this->DB_USER = $_ENV['DB_USER'];
+        $this->DB_PASSWORD = $_ENV['DB_PASSWORD'];
+    }
 
     // GET CONNECTION TO DB
     public function getConnection(): PDO
     {
-        $pdo = new PDO('mysql:host=' . $this::DB_HOST . ';charset=utf8mb4;dbname=' . $this::DB_NAME, $this::DB_USER, $this::DB_PASSWORD);
+        $pdo = new PDO("mysql:host=$this->DB_HOST;charset=utf8mb4;dbname=$this->DB_NAME", $this->DB_USER, $this->DB_PASSWORD);
         return $pdo;
     }
 }
